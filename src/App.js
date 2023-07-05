02b4dc6477677740d74a98ea95c7341bbdf05079
Italo Bandeira <italobpl94@gmail.com>
2023-07-05 19:40:59 -0300

    Add header component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react'
-import Navbar from './components/navbar/navbar'
+import Navbar from './components/Navbar/navbar'
+import Header from './components/Header/header'
 
 function App() {
   return (
     <div>
       <Navbar/>
+      <Header/>
     </div>
   );
 }
