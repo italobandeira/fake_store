d03d0d1f2e698214148ed941835f4add4e377cac
Italo Bandeira <italobpl94@gmail.com>
2023-07-06 01:57:16 -0300

    Adding category filter

--- src/components/Products/products.js
+++ src/components/Products/products.js
@@ -1,15 +1,41 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { Link } from 'react-router-dom'
 
-function Products({ products = [] }) {
+function Products() {
+
+  const [products, setProducts] = useState([])
+  const [filter, setFilter] = useState(products);
+
+  useEffect(() => {
+    const fetchProducts = async () => {
+      const response = await fetch('https://fakestoreapi.com/products')
+      const data = await response.json()
+      setFilter(data);
+      setProducts(data)
+    }
+    fetchProducts()
+  }, [])
+
+  const filterProduct = (cat) => {
+    const updatedList = products.filter((x) => x.category === cat);
+    setFilter(updatedList);
+  }
+
   return (
     <div class="container">
       <h1 className="text-center display-5 fw-bolder mt-3">Products</h1>
       <hr className="border border-dark"></hr>
+      <div className="buttons d-flex justify-content-center mb-5 pb-5 col-12">
+        <button className="btn btn-outline-dark me-2" onClick={() => setFilter(products)}>All</button>
+        <button className="btn btn-outline-dark me-2" onClick={() => filterProduct("men's clothing")}>Men's Clothing</button>
+        <button className="btn btn-outline-dark me-2" onClick={() => filterProduct("women's clothing")}> Women's Clothing </button>
+        <button className="btn btn-outline-dark me-2" onClick={() => filterProduct("jewelery")}>Jewelery</button>
+        <button className="btn btn-outline-dark me-2" onClick={() => filterProduct("electronics")}>Electronic</button>
+      </div>
       <div className="row">
         <div class="card-group">
           {
-            products.map((product) => {
+            filter.map((product) => {
               const { id, title, price, category, image } = product
               return (
                 <Link to={`/products/${id}`} className="col-12 col-md-6 col-lg-3 mb-3 text-decoration-none">
--- src/modules/Home/home.js
+++ src/modules/Home/home.js
@@ -1,24 +1,12 @@
-import React, { useEffect, useState } from 'react'
 import Products from '../../components/Products/products'
 
 function Home() {
-  const [products, setProducts] = useState([])
-  useEffect(() => {
-    const fetchProducts = async () => {
-      const response = await fetch('https://fakestoreapi.com/products')
-      const data = await response.json()
-      setProducts(data)
-    }
-    fetchProducts()
-  }, [])
-
 
   return (
     <div className='container border mt-5'>
       {
-        <Products products = {products} />
+        <Products/>
       }
-      {/* <Products/> */}
     </div>
   )
 }
